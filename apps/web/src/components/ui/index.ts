5ad97779c0ef08e57543f60423d08d69038c8501
victorlirafront <victorliradeveloper@gmail.com>
2025-10-14 00:37:56 -0300

    feat: add ui reusable components

---
+++ apps/web/src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { Button } from './Button';
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './Card';
+export { Badge } from './Badge';
+export { Input } from './Input';
+export { Select } from './Select';
+export { Textarea } from './Textarea';
+
