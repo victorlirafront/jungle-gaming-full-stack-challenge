a7c26fd99b2fcfcb3ab163a47d10f5ddcc8e7d90
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-10-20 22:19:13 -0300

    feat: add simple health check to api-gateway

--- apps/api-gateway/src/modules/health/health.controller.ts
+++ apps/api-gateway/src/modules/health/health.controller.ts
@@ -1,24 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheck, HealthCheckService, MicroserviceHealthIndicator } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
-  constructor(
-    private health: HealthCheckService,
-    private microservice: MicroserviceHealthIndicator,
-  ) {}
-
   @Get()
-  @HealthCheck()
   check() {
-    return this.health.check([
-      () => this.microservice.pingCheck('rabbitmq', {
-        transport: 1,
-        options: {
-          urls: [process.env.RABBITMQ_URL || 'amqp://admin:admin@rabbitmq:5672'],
-        },
-      }),
-    ]);
+    return {
+      status: 'ok',
+      timestamp: new Date().toISOString(),
+      service: 'api-gateway',
+    };
   }
 }
 
